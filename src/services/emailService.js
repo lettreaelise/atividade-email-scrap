01e9dc31f75c52bd9846c8d28c78c9239f6eb117
mathisawesome <93688312+lettreaelise@users.noreply.github.com>
2024-12-03 21:51:17 -0300

    Update emailService.js

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,17 +1,17 @@
 const nodemailer = require('nodemailer');
-require('dotenv').config(); // Certifique-se de que o dotenv está sendo carregado
+require('dotenv').config();
 
 const sendEmail = async (data) => {
   try {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER, // Seu e-mail
-        pass: process.env.EMAIL_PASS, // Sua senha de app
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
-    // Criação do corpo do e-mail com a tabela HTML
+    
     let htmlContent = `
       <h1>Eletrônicos na Amazon</h1>
       <table border="1" cellpadding="5" cellspacing="0" style="border-collapse: collapse;">
@@ -25,7 +25,7 @@ const sendEmail = async (data) => {
         <tbody>
     `;
 
-    // Preenchendo a tabela com os dados obtidos do scraping
+    
     data.forEach(item => {
       htmlContent += `
         <tr>
@@ -41,20 +41,20 @@ const sendEmail = async (data) => {
       </table>
     `;
 
-    // Definindo as opções do e-mail
+    
     const mailOptions = {
       from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER, // Envia para o mesmo e-mail
+      to: process.env.EMAIL_USER,
       subject: 'Dados do Scraper',
-      html: htmlContent, // Corpo do e-mail em HTML
+      html: htmlContent,
     };
 
-    // Enviar o e-mail
+    
     await transporter.sendMail(mailOptions);
     console.log('E-mail enviado com sucesso!');
   } catch (error) {
     console.error('Erro ao enviar e-mail:', error.message);
-    throw error; // Lança o erro para ser capturado na função principal
+    throw error;
   }
 };
 
