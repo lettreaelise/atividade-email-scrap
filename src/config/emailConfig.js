7944aa2eafeea0146f2f8e2b10ece1a06ed4c762
mathisawesome <93688312+lettreaelise@users.noreply.github.com>
2024-12-03 21:49:24 -0300

    Update emailConfig.js

--- src/config/emailConfig.js
+++ src/config/emailConfig.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER, // Seu email (Ex: fds23oficial@gmail.com)
-    pass: process.env.EMAIL_PASSWORD, // Senha de aplicativo ou senha normal
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
   },
 };
